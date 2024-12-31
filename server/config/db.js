283c57b81ab2b369044c8c2abffae2784683be44
Gaurav Jain <2022kuec2013@iiitkota.ac.in>
2024-12-31 23:38:42 +0530

    third commit

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async ()=>{
 
     mongoose.connection.on('connected',()=>console.log('database connected'));
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/temporary`)
 }
 
 export default connectDB;
