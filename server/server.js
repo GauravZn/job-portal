66c239275152e63387a3b5e8dcf0c0ca34c691a5
Gaurav Jain <2022kuec2013@iiitkota.ac.in>
2025-01-01 00:06:26 +0530

    fourth commit

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
+import './config/instrument.js'
 import express from 'express';
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js';
-import './config/instrument.js'
 import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from './controllers/webhooks.js';
 
